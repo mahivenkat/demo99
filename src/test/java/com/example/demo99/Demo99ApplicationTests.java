989a5d935995a0b88f83f766c449a5e79c93f984
KamaniVenkat <kamanivenkat@gmail.com>
2022-07-29 16:09:46 +0530

    simple mockmvc unittest is success

--- src/test/java/com/example/demo99/Demo99ApplicationTests.java
+++ src/test/java/com/example/demo99/Demo99ApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.demo99;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Demo99ApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
+
 
 	@Test
-	void contextLoads() {
+	public void getAllEmployees() throws Exception {
+		mockMvc.perform(get("http://localhost:8080/api/v1/employee/employees")).andDo(print()).
+				andExpect(status().isOk());
 	}
 
 }
